7c3b6d7aa28fd732dc0ca314a2cec2f2be0d1362
dipak <d74252835@gmail.com>
2025-07-04 18:58:46 +0530

    done my side

--- frontend/src/landing_page/signup/Emailinput.jsx
+++ frontend/src/landing_page/signup/Emailinput.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../signup/Signup.css";
+import "../signup/signup.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
